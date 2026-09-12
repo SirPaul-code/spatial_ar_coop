f4bb65c6321e4e2216b535204836e89515785696
Pavol Duplinsky <79102860+SirPaul-code@users.noreply.github.com>
2026-09-12 16:06:06 +0200

    build(android): align Kotlin toolchain with StableAR

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,4 +1,6 @@
 plugins {
     id("com.android.application") version "8.10.1" apply false
-    id("org.jetbrains.kotlin.android") version "1.9.24" apply false
+    id("com.android.library") version "8.10.1" apply false
+    id("org.jetbrains.kotlin.android") version "2.3.20" apply false
+    id("org.jetbrains.kotlin.jvm") version "2.3.20" apply false
 }
