c41f1ed49fed8f97caf2f0e70a3a7dbe94a2be88
Pavol Duplinsky <79102860+SirPaul-code@users.noreply.github.com>
2026-09-12 18:40:56 +0200

    build(p2p): include StableAR Android modules

--- android/settings.gradle.kts
+++ android/settings.gradle.kts
@@ -15,4 +15,8 @@ dependencyResolutionManagement {
 }
 
 rootProject.name = "SpatialNoMap"
-include(":app")
+include(":app", ":core", ":arcore", ":vision", ":native-vision-android")
+project(":core").projectDir = file("../sdk/core")
+project(":arcore").projectDir = file("../sdk/arcore")
+project(":vision").projectDir = file("../sdk/vision")
+project(":native-vision-android").projectDir = file("../sdk/native-vision-android")
